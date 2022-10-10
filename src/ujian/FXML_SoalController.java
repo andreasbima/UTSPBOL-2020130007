86d10162ee5142274717c59a0c04c4de8f03d0cf
Andreas Bima <andreas.bima10@gmail.com>
2022-10-10 12:16:37 +0700

    tambah soal

--- src/ujian/FXML_SoalController.java
+++ src/ujian/FXML_SoalController.java
@@ -32,6 +32,42 @@ public class FXML_SoalController implements Initializable {
     private TextField txt01;
     @FXML
     private Button btncek1;
+    @FXML
+    private TextField txt02;
+    @FXML
+    private TextField txt03;
+    @FXML
+    private TextField txt04;
+    @FXML
+    private TextField txt05;
+    @FXML
+    private TextField txt10;
+    @FXML
+    private TextField txt09;
+    @FXML
+    private TextField txt08;
+    @FXML
+    private TextField txt07;
+    @FXML
+    private TextField txt06;
+    @FXML
+    private Button btncek2;
+    @FXML
+    private Button btncek3;
+    @FXML
+    private Button btncek4;
+    @FXML
+    private Button btncek5;
+    @FXML
+    private Button btncek6;
+    @FXML
+    private Button btncek7;
+    @FXML
+    private Button btncek8;
+    @FXML
+    private Button btncek9;
+    @FXML
+    private Button btncek10;
 
     /**
      * Initializes the controller class.
@@ -70,6 +106,82 @@ public class FXML_SoalController implements Initializable {
             JOptionPane.showMessageDialog(null, "Wrong");
             txt01.setText("");
         }
+    }
+
+    @FXML
+    private void cek2klik(ActionEvent event) {
+        if(txt02.getText().trim().equalsIgnoreCase("pasifik")){
+            JOptionPane.showMessageDialog(null, "Correct");
+        }else if(txt02.getText().equals("")){
+            JOptionPane.showMessageDialog(null, "Fill in the answer");
+        }else{
+            JOptionPane.showMessageDialog(null, "Wrong");
+            txt02.setText("");
+        }
+    }
+
+    @FXML
+    private void cek3klik(ActionEvent event) {
+        if(txt03.getText().trim().equalsIgnoreCase("sungai")){
+            JOptionPane.showMessageDialog(null, "Correct");
+        }else if(txt03.getText().equals("")){
+            JOptionPane.showMessageDialog(null, "Fill in the answer");
+        }else{
+            JOptionPane.showMessageDialog(null, "Wrong");
+            txt03.setText("");
+        }
+    }
+
+    @FXML
+    private void cek4klik(ActionEvent event) {
+        if(txt04.getText().trim().equalsIgnoreCase("oseanografi")){
+            JOptionPane.showMessageDialog(null, "Correct");
+        }else if(txt04.getText().equals("")){
+            JOptionPane.showMessageDialog(null, "Fill in the answer");
+        }else{
+            JOptionPane.showMessageDialog(null, "Wrong");
+            txt04.setText("");
+        }
+    }
+
+    @FXML
+    private void cek5klik(ActionEvent event) {
+        if(txt05.getText().trim().equalsIgnoreCase("vulkanik")){
+            JOptionPane.showMessageDialog(null, "Correct");
+        }else if(txt05.getText().equals("")){
+            JOptionPane.showMessageDialog(null, "Fill in the answer");
+        }else{
+            JOptionPane.showMessageDialog(null, "Wrong");
+            txt05.setText("");
+        }
+    }
+
+    @FXML
+    private void cek6klik(ActionEvent event) {
+        if(txt06.getText().trim().equalsIgnoreCase("tektonik")){
+            JOptionPane.showMessageDialog(null, "Correct");
+        }else if(txt06.getText().equals("")){
+            JOptionPane.showMessageDialog(null, "Fill in the answer");
+        }else{
+            JOptionPane.showMessageDialog(null, "Wrong");
+            txt06.setText("");
+        }
+    }
+
+    @FXML
+    private void cek7klik(ActionEvent event) {
+    }
+
+    @FXML
+    private void cek8klik(ActionEvent event) {
+    }
+
+    @FXML
+    private void cek9klik(ActionEvent event) {
+    }
+
+    @FXML
+    private void cek10klik(ActionEvent event) {
     }
     
 }
